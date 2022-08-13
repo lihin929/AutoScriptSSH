410ce02f06ace19aa7979e6eae9aa4e4c7aeedb8
lihin929 <85750131+lihin929@users.noreply.github.com>
2022-08-14 02:58:21 +0700

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 clear
-echo Installing Websocket-SSH AUTOSCRIPT BY JOMBLOSSH
+echo Installing Websocket-SSH AUTOSCRIPT BY Lihin
 sleep 1
 echo Cek Hak Akses...
 sleep 0.5
 cd
 #Install system auto run
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #Enable & Start ws-ovpn service
 systemctl enable ws-ovpn.service
