6309b28246c063214c6f84d804ab1eede44db82c
lihin929 <85750131+lihin929@users.noreply.github.com>
2022-08-14 03:45:20 +0700

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/rclone.conf"
 apt install msmtp-mta ca-certificates bsd-mailx -y
 cat<<EOF>>/etc/msmtprc
 defaults
@@ -13,18 +13,18 @@ account default
 host smtp.gmail.com
 port 587
 auth on
-user corloussss@gmail.com
-from corloussss@gmail.com
-password vmlpmbagegqzhsqy
+user dadang6922@gmail.com
+from 
+password Tes1234567@
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/backup.sh"
-wget -O bckp "https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/strt.sh"
+wget -O autobackup "https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/strt.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
