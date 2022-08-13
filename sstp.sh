f5fff8b52859341a9faa6e2821087a70bf4e3d0b
lihin929 <85750131+lihin929@users.noreply.github.com>
2022-08-14 03:33:05 +0700

    Update sstp.sh

--- sstp.sh
+++ sstp.sh
@@ -64,10 +64,10 @@ netfilter-persistent save
 netfilter-persistent reload
 
 #input perintah sstp
-wget -O /usr/bin/addsstp https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/addsstp.sh && chmod +x /usr/bin/addsstp
-wget -O /usr/bin/delsstp https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/delsstp.sh && chmod +x /usr/bin/delsstp
-wget -O /usr/bin/ceksstp https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/ceksstp.sh && chmod +x /usr/bin/ceksstp
-wget -O /usr/bin/xp-sstp https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/xp-sstp.sh && chmod +x /usr/bin/xp-sstp
-wget -O /usr/bin/renewsstp https://raw.githubusercontent.com/izinrecode/AutoScriptSSH/main/renewsstp.sh && chmod +x /usr/bin/renewsstp
+wget -O /usr/bin/addsstp https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/addsstp.sh && chmod +x /usr/bin/addsstp
+wget -O /usr/bin/delsstp https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/delsstp.sh && chmod +x /usr/bin/delsstp
+wget -O /usr/bin/ceksstp https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/ceksstp.sh && chmod +x /usr/bin/ceksstp
+wget -O /usr/bin/xp-sstp https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/xp-sstp.sh && chmod +x /usr/bin/xp-sstp
+wget -O /usr/bin/renewsstp https://raw.githubusercontent.com/lihin929/AutoScriptSSH/main/renewsstp.sh && chmod +x /usr/bin/renewsstp
 rm -f /root/sstp.sh
 echo "0 0 * * * root xp-sstp" >> /etc/crontab
