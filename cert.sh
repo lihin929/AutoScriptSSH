c8900a42657f4fc60647de0875c7d9b03e6d3922
lihin929 <85750131+lihin929@users.noreply.github.com>
2022-08-14 14:04:34 +0700

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -4,6 +4,7 @@ sleep 0.5
 domain=$(cat /etc/v2ray/domain)
 systemctl stop v2ray
 systemctl stop v2ray@none
+/root/.acme.sh/acme.sh --register-account -m budiamanfamily@gmail.com
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 sleep 2
